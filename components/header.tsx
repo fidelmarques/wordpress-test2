12394f064b8a358608d13bd45ba316350aa2051c
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2023-05-01 22:37:01 -0300

    feat: motion add

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,29 @@
 import Link from 'next/link'
+import { TbMenu2, TbSearch } from "react-icons/tb"
 
 export default function Header() {
   return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 italic">
-      <Link href="/" className="hover:underline">
-        sonora.
+    <div className='flex-row flex items-center justify-between transform transition-all py-4'>
+      <Link
+        href={""}
+        className="hover:underline"
+      >
+        <TbMenu2 className=' text-3xl' />
       </Link>
 
-    </h2>
+      <h2 className="md:text-4xl sm:text-6xl font-bold tracking-tighter leading-tight md:pr-8 italic">
+        <Link href="/" className="hover:underline">
+          sonora.
+        </Link>
+      </h2>
+
+      <Link
+        href={""}
+        className="hover:underline"
+      >
+        <TbSearch className=' text-3xl' />
+      </Link>
+
+    </div>
   )
 }
